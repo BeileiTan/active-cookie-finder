0a7bed628d1d91a62230cede86bc55ab839adab9
BeileiTan <beilei0508@gmail.com>
2024-06-03 21:54:50 -0700

    first commit

---
+++ src/main/java/com/springdemo/activecookiefinder/ActiveCookieFinderApplication.java
@@ -0,0 +1,13 @@
+package com.springdemo.activecookiefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveCookieFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActiveCookieFinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springdemo/activecookiefinder/ActiveCookieFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdemo.activecookiefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveCookieFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
